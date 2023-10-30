ff69dfa3092303b8b092d0bec60d4b048177b623
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-10-30 20:47:25 +0300

    add file structure & basic skeleton

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
+
+require("express-async-errors");
+
+app.use(express.json());
+
+// Error handler
+app.use(require("./middlewares/errorHandler"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running: http://127.0.0.1:${PORT}`);
+});
