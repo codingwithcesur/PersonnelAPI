ce62c3d420de08646de1aa3345d7819917621f56
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-10-30 21:08:27 +0300

    add department model

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,6 +1,20 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+"use strict";
+
+const { mongoose } = require("../configs/dbConnection");
+
 /* ------------------------------------------------------- */
+
+const departmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { collection: "departments", timestamps: true }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Department", departmentSchema);
