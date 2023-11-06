5b00ba9087ac83fa3db2651590bd47280dc5fe90
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-06 21:27:19 +0300

    add CRUD for department

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -1,4 +1,43 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
+"use strict";
+
+const Department = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Department);
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await res.createModel(Department, req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Department.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await Department.updateOne({ _id: req.params.id }, req.body);
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Department.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Department.deleteOne({ _id: req.params.id });
+    const isDeleted = data.deletedCount >= 1 ? true : false;
+    res.status(isDeleted ? 204 : 404).send({
+      error: isDeleted,
+      data,
+    });
+  },
+};
