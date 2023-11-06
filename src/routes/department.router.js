75bff3c538533e91c72b7d320e646045960479e0
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-06 21:31:15 +0300

    add routing for department

--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -1,11 +1,15 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-const router = require('express').Router()
+"use strict";
+
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+const department = require("../controllers/department.controller");
 
-
-
+// URL: /departments
+router.route("/").get(department.list).post(department.create);
+router
+  .route("/:id")
+  .get(department.read)
+  .put(department.update)
+  .delete(department.delete);
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
