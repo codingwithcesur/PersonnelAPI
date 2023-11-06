2b281ccc58fc4cd9e1b1fcaa61ea656101907692
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-06 21:47:05 +0300

    add CRUD for personnel

--- src/controllers/personnel.controller.js
+++ src/controllers/personnel.controller.js
@@ -1,4 +1,43 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
+"use strict";
+
+const Personnel = require("../models/personnel.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Personnel);
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await res.createModel(Personnel, req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Personnel.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await Personnel.updateOne({ _id: req.params.id }, req.body);
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Personnel.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Personnel.deleteOne({ _id: req.params.id });
+    const isDeleted = data.deletedCount >= 1 ? true : false;
+    res.status(isDeleted ? 204 : 404).send({
+      error: isDeleted,
+      data,
+    });
+  },
+};
