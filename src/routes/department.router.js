c83e3fa80e991bf9f3c54c6680ae742e17985369
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-08 20:27:11 +0300

    apply permission rules for department

--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -3,15 +3,19 @@
 const router = require("express").Router();
 /* ------------------------------------------------------- */
 const department = require("../controllers/department.controller");
+const permissions = require("../middlewares/permissions");
 
 // URL: /departments
-router.route("/").get(department.list).post(department.create);
+router
+  .route("/")
+  .get(permissions.isLogin, department.list)
+  .post(permissions.isAdmin, department.create);
 router
   .route("/:id")
-  .get(department.read)
-  .put(department.update)
-  .patch(department.update)
-  .delete(department.delete);
+  .get(permissions.isAdminOrLead, department.read)
+  .put(permissions.isAdminOrLead, department.update)
+  .patch(permissions.isAdminOrLead, department.update)
+  .delete(permissions.isAdmin, department.delete);
 
 router.get("/:id/personnels", department.personnels);
 /* ------------------------------------------------------- */
